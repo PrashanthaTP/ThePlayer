b51f7cd436119b917048dbd7b55d3d31a138a877
PrashanthaTP <tpprashantha@gmail.com>
2026-06-22 21:13:03 +0530

    Dev: Set game window height to 480px

--- src/constants.cpp
+++ src/constants.cpp
@@ -2,7 +2,7 @@
 
 const GameSettings GSettings = {.name = "ThePlayer",
                                 .width = 640,
-                                .height = 640,
+                                .height = 480,
                                 .groundY = 320.0f + 40 + 20};
 
 const Colors GColors;
