d70b70967aa8868864f8bd01b655fdb289dea47e
PrashanthaTP <tpprashantha@gmail.com>
2026-06-28 19:52:44 +0530

    Dev: Sketch coin

---
+++ inc/coin.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <sfml/Graphics.hpp>
+
+class Coin {
+  private:
+    int _val;
+    float _radius;
+    sf::CircleShape _obj;
+  public:
+    Coin(int val, float radius);
+    void draw(sf::RenderWindow &window);
+};
+
+
+class CoinManager {
+    public:
+    CoinManager();
+    void update(sf::RenderWindow &window, float time);
+    void draw(sf::RenderWindow &window);
+};
---
+++ src/coin.cpp
@@ -0,0 +1,18 @@
+#include "coin.hpp"
+
+Coin::Coin(int val, float radius)
+    : _val(val),
+      _radius(radius),
+      _obj(sf::CircleShape(radius)) {
+}
+
+void Coin::draw(sf::RenderWindow &window) {
+    window.draw(_obj);
+}
+
+
+CoinManager::CoinManager(){}
+
+void CoinManager::update(sf::RenderWindow &window, float time){}
+
+void CoinManager::draw(sf::RenderWindow &window){}
